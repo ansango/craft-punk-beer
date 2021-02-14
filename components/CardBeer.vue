7a7ca7e7bdf46e448b5450b9772af70601cac335
Anibal Santos <anibalsantosgo@gmail.com>
2021-02-14 17:49:27 +0100

    tailwind

--- components/CardBeer.vue
+++ components/CardBeer.vue
@@ -1,25 +1,63 @@
 <template>
-  <div class="flex flex-wrap justify-items-center container m-auto">
+  <div class="">
     <div
-      v-for="beer in beers"
-      :key="beer.id"
-      class="w-72 bg-white shadow-lg rounded-lg overflow-hidden my-20"
+      class="py-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-10"
     >
-      <nuxt-link :to="`/beers/${beer.route}`">go to</nuxt-link>
-      <h3 class="text-gray-900 font-bold text-2xl uppercase">
-        {{ beer.name }}
-      </h3>
-      <p class="text-gray-600 text-sm mt-1 capitalize">
-        {{ beer.subtitle }}
-      </p>
-      <p class="text-gray-600 text-sm mt-1 capitalize">{{ beer.section }}</p>
-      <p class="capitalize"><span>ABV:</span>{{ beer.abv }} <span>%</span></p>
-      <div class="cover-container">
-        <img
-          class="h-full w-full object-cover mt-2"
-          :src="beer.imgURL"
-          :alt="beer.name"
-        />
+      <div
+        v-for="beer in beers"
+        :key="beer.id"
+        class="flex w-full bg-white shadow-lg rounded-lg overflow-hidden"
+      >
+        <div
+          class="w-1/3 bg-cover"
+          :style="`background-image: url('${beer.imgURL}')`"
+        ></div>
+        <div class="w-2/3 p-4">
+          <h1 class="text-gray-900 font-bold text-2xl">{{ beer.name }}</h1>
+
+          <p class="mt-2 text-gray-600 text-sm">
+            {{ beer.subtitle }}
+          </p>
+          <p class="mt-2 text-gray-600 text-sm">
+            {{ beer.abv }} <span>%</span>
+          </p>
+
+          <div class="flex item-center mt-2">
+            <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
+              <path
+                d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+              />
+            </svg>
+            <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
+              <path
+                d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+              />
+            </svg>
+            <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
+              <path
+                d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+              />
+            </svg>
+            <svg class="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
+              <path
+                d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+              />
+            </svg>
+            <svg class="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
+              <path
+                d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+              />
+            </svg>
+          </div>
+          <div class="flex item-center justify-between mt-3">
+            <h1 class="text-gray-700 font-bold text-xl">{{ beer.section }}</h1>
+            <nuxt-link
+              class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded"
+              :to="`/beers/${beer.route}`"
+              >Info</nuxt-link
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
